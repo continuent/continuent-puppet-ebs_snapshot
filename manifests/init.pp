29f7b459a90a4b0fea37533f529e676322012baf
narmitag <neil@neila.co.uk>
2014-01-08 08:28:16 +0000

    re-sync from private repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,6 +20,8 @@
 class continuent_ebs_snapshot {
 
 
+
+
 }
 
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,24 +1,8 @@
-# === Copyright
-#
-# Copyright 2013 Continuent Inc.
-#
-# === License
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#		http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 class continuent_ebs_snapshot::install(
 ) {
+
+
+
 
     file { '/tmp/install_binaries':
       ensure => file,
@@ -30,4 +14,16 @@ class continuent_ebs_snapshot::install(
       command => "/tmp/install_binaries",
       creates => "/usr/local/bin/ec2-consistent-snapshot"
     }
+ 
+   file { '/usr/local/bin/ebs_snap':
+      ensure => file,
+      owner => 'root',
+      mode => 700,
+      content => template('continuent_ebs_snapshot/ebs_snap.erb')
+    }
+
 }
+
+
+
+
